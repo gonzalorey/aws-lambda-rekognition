2467ce33f7f64ced230d3b0233d9a482d6c88061
Gonzalo M. Rey <gonzalo.rey@restorando.com>
2017-06-26 15:25:46 -0300

    Added a functional version with multer

--- lambda.js
+++ lambda.js
@@ -7,8 +7,6 @@ if (!AWS.config.region) {		// ugly hack :´(
   })
 }
 var rekognition = new AWS.Rekognition()
-var form = new (require('multiparty')).Form({encoding: 'base64'})
-var fs = require('fs')
 
 console.log('Loading function...')
 
@@ -18,66 +16,32 @@ console.log('Loading function...')
  */
 exports.handler = (event, context, callback) => {
 	console.log(event)
-	// console.log(context)
 }
 
 exports.parseImage = (req, res) => {
-	var buffer
-	var params
+	var params = {
+	  Image: {
+	      Bytes: req.file.buffer
+	  },
+	  MinConfidence: 50
+	}
 
-	form.on('error', (err) => {
-		console.log('Error parsing form: ' + err.stack)
-	})
+	rekognition.detectLabels(params, (err, rekognitionRes) => {
+		if (err) {
+		  console.log(err, err.stack)
+			rekognitionRes.status(500).send(err.stack)
+		} else {
+		  console.log("-------- START: Object and scene detection --------")
+		  var labels = rekognitionRes.Labels
+		  var data = {}
+		  for(var i=0; i<labels.length; i++) {
+		      console.log('Name =' + labels[i].Name + ', Confidence =' + labels[i].Confidence)
+		      data[labels[i].Name] = labels[i].Confidence + ''
+		  }
+		  console.log("-------- END: Object and scene detection --------")
+		  console.log("\n")
 
-	form.on('part', (part) => {
-		console.log(`Part received: ${JSON.stringify(part)}`)
-		if (!part.filename) {
-			console.log('filename not defined, ignoring file content...')
-
-			return;
+			res.status(200).send(data)
 		}
-
-		// console.log(`Offset: ${part._readableState}`)
-		console.log(`Part received: ${JSON.stringify(part)}`)
-
-		// if (part._readableState.objectMode) {
-		// 	buffer = Buffer.concat([buffer, part.buffer.head])
-		// }
-
-		params = {
-      Image: {
-        Bytes: part
-      },
-      MinConfidence: 50
-    }
-
-		part.resume()
-
-		part.on('error', function(err) {
-     console.log(`Error received when parsing part: ${err}`)
-  	})
-	})
-
-	form.on('close', () => {
-		console.log('Upload completed!')
-
-		rekognition.detectLabels(params, (err) => {
-			if (err) {
-			  console.log(err, err.stack) // an error occurred
-				res.status(500).send(err.stack)
-			} else {
-			  console.log("-------- START: Object and scene detection --------")
-			  var labels = res.Labels
-			  var data = {}
-			  for(var i=0; i<labels.length; i++) {
-			      console.log('Name =' + labels[i].Name + ', Confidence =' + labels[i].Confidence)
-			      data[labels[i].Name] = labels[i].Confidence + ''
-			  }
-			  console.log("-------- END: Object and scene detection --------")
-			  console.log("\n")
-			}
-		})
-	})
-
-	form.parse(req)
+  })
 }
--- server.js
+++ server.js
@@ -3,14 +3,27 @@ var app = express();
 
 var lambda = require('./lambda.js');
 
-app.get('/', function (req, res) {
+var multer = require('multer')
+let upload = multer({ storage: multer.memoryStorage() })
+
+app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-app.post('/lambda', function (req, res) {
+app.post('/lambda', upload.single('image'), (req, res) => {
   lambda.parseImage(req, res);
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Listening on port 3000!');
 });
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+
+  if(req.path == '/lambda' && !req.file) {
+    res.status(400).send(`'image' parameter wrong or missing!`)
+  } else {
+    res.status(500).send('Something broke!')
+  }
+})
