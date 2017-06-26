c094a477bd8f17d1ebc9735494c5bec463b15203
Gonzalo M. Rey <gonzalo.rey@restorando.com>
2017-06-26 11:36:52 -0300

    Uploading progress...
    Failing attempt to read with fs.readFileSync and then upload image, gonna try another approach.

--- lambda.js
+++ lambda.js
@@ -1,5 +1,23 @@
 'use strict';
 
+var File = require('file-api').File;
+
+var AWS = require('aws-sdk');
+if (!AWS.config.region) {		// ugly hack :´(
+  AWS.config.update({
+    region: 'eu-west-1'
+  });
+}
+var rekognition = new AWS.Rekognition();
+
+var multiparty = require('multiparty');
+var form = new multiparty.Form();
+
+var FileReader = require('filereader');
+var fileReader = new FileReader();
+
+var fs = require('fs')
+
 console.log('Loading function...');
 
 /**
@@ -9,27 +27,89 @@ console.log('Loading function...');
 exports.handler = (event, context, callback) => {
 	console.log(event);
 	// console.log(context);
+};
 
-	// Lets asume we already have the image in base64
-	var imageBytes;
+exports.parseImage = (req, res) => {
+	form.parse(req);
 
-	// Build the rekognition request
+	form.on('error', (err) => {
+		console.log('Error parsing form: ' + err.stack);
+	});
+
+	form.on('part', (part) => {
+		part.resume();
+		// if (!part.filename) {
+		// 	// filename is not defined when this is a field and not a file
+		// 	console.log('got field named ' + part.name);
+		// 	// ignore field's content
+		// 	part.resume();
+		// }
+		//
+		// if (part.filename) {
+		// 	// filename is defined when this is a file
+		// 	console.log('got file named ' + part.name);
+		// 	// ignore file's content here
+		// 	part.resume();
+		// }
+		//
+		// part.on('error', function(err) {
+		// 	console.log('Error processing file part: ' + err.stack);
+		// });
+	});
+
+	form.on('file', (name, file) => {
+		console.log('Uploaded file: ' + name)
+
+		rekognizeImage(file, (err, labels) => {
+			console.log('rekognizing image...')
+
+			if (err) {
+				console.log(err, err.stack)
+				res.status(500).send(err.stack)
+		  } else {
+				console.log(data)
+				res.end('Labels: ' + labels)
+			}
+		})
+
+	})
+
+	// form.on('close', function() {
+	// 	console.log('Upload completed!');
+	// 	res.setHeader('Content-Type', 'text/plain');
+	// 	res.end('Received file');
+	// });
+};
+
+function rekognizeImage(file, callback) {
+	console.log(file)
+
+	// fs.readFile(file.path, { 'encoding': 'base64' }, (err, data) => {
+	// 	console.log('reading file...')
+	//
+	// 	if(err) {
+	// 		console.log(err)
+	// 	} else {
+	// 		console.log('file successfuly read, calling rekognition...')
+	//
+	// 		var rekognitionRequest = {
+	// 			'Image': {
+	// 				'Bytes': data
+	// 			}
+	// 		}
+	//
+	// 		rekognition.detectLabels(rekognitionRequest, callback)
+	//
+	// 	}
+	// })
+
+	var bitmap = fs.readFileSync(file.path, { 'encoding': 'base64' })
+
 	var rekognitionRequest = {
-		"image": {
-			"Bytes": imageBytes
+		'Image': {
+			'Bytes': new Buffer(bitmap).toString('base64')
 		}
 	}
 
-	/** An image can be extracted from an S3 bucket, or received through base64 encoded bytes,
-	 *  an example for the second one can be founde here:
-	 *  - http://docs.aws.amazon.com/rekognition/latest/dg/example4.html
-	 *  - http://docs.aws.amazon.com/rekognition/latest/dg/get-started-exercise-detect-labels.html
-	 */
-	var labels = rekognitionClient.detectLabels(rekognitionRequest);
-
-	if (labels.isNotEmpty()) {
-		logger.log(labels);
-	} else {
-		logger.log("No labels returned. Not saving to ES");
-	}
-};
+	rekognition.detectLabels(rekognitionRequest, callback)
+}
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-// var lambda = require('./lambda.js');
+var lambda = require('./lambda.js');
 
 app.get('/', function (req, res) {
-  res.send('hello world')
-})
+  res.send('hello world');
+});
 
 app.post('/lambda', function (req, res) {
-  res.send('POST request to the homepage')
-})
+  lambda.parseImage(req, res);
+});
 
 app.listen(3000, function () {
-  console.log('Listening on port 3000!')
-})
+  console.log('Listening on port 3000!');
+});
