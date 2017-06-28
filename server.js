b1138b88b89b34054c2edaa56050c84553915245
Gonzalo M. Rey <gonzalo.rey@restorando.com>
2017-06-28 11:46:47 -0300

    Added support for uploading to S3
    Now, an image can be uploaded to S3, and then call Rekognition using that image directly.

--- server.js
+++ server.js
@@ -2,25 +2,32 @@ var app = require('express')()
 
 var lambda = require('./lambda.js')
 
+var multer = require('multer')
+var upload = multer({ storage: multer.memoryStorage() })
+
 const BUCKET_NAME = 'gonzalorey-test-bucket'
 const IMAGE_NAME = 'elmo-face.jpg'
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+	res.send('hello world')
 })
 
 app.get('/lambda', (req, res) => {
-  lambda.parseImage(BUCKET_NAME, IMAGE_NAME, res)
+	lambda.analyzeImage(BUCKET_NAME, IMAGE_NAME, res)
+})
+
+app.post('/lambda', upload.single('image'), (req, res) => {
+	lambda.uploadImage(BUCKET_NAME, req.file, res)
 })
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000!')
+	console.log('Listening on port 3000!')
 })
 
 app.use((err, req, res, next) => {
-  console.error(err.stack)
+	console.error(err.stack)
 
-  if(err) {
-    res.status(500).send('Something broke!')
-  }
+	if(err) {
+		res.status(500).send('Something broke!')
+	}
 })
